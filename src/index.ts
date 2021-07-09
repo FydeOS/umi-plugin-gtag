595ff484ecb568c2c78bfecc2cca93bc4f7cbb8e
yudong <yudong@fydeos.io>
2021-07-09 21:27:56 +0800

    remove SEND_DATA_NAME

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,13 @@ export default function(api: IApi) {
       schema(joi) {
         return joi.object({
           GA_MEASUREMENT_ID: joi.string(),
-          SEND_DATA_NAME: joi.string(),
         });
       },
     },
   });
-  const gtagTpl = function(id: string, key: string) {
+  const gtagTpl = function(id: string) {
     return `
     (function(){
-      var data =  window.g_initialProps["${key}"] || window["${key}"]
       var script = document.createElement('script');
       script.src = 'https://www.googletagmanager.com/gtag/js?id=${id}';
       script.async = true;
@@ -29,19 +27,18 @@ export default function(api: IApi) {
         };
         window.gtag('js', new Date());
         window.gtag('config', '${id}');
-        data && window.gtag('set', data)
       };
       document.getElementsByTagName('head')[0].appendChild(script);
     })();
   `;
   };
   if (api.userConfig.gtag) {
-    const { GA_MEASUREMENT_ID, SEND_DATA_NAME } = api.userConfig.gtag;
+    const { GA_MEASUREMENT_ID } = api.userConfig.gtag;
     if (api.env !== 'development') {
       if (GA_MEASUREMENT_ID) {
         api.addHTMLScripts(() => [
           {
-            content: gtagTpl(GA_MEASUREMENT_ID, SEND_DATA_NAME),
+            content: gtagTpl(GA_MEASUREMENT_ID),
           },
         ]);
       }
